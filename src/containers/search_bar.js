95d5d567740b589f0d7801c4add8ad9ba3f1d1d9
Omer Tarik <omertarik96@gmail.com>
2017-08-30 10:30:20 -0500

    Lesson 58: promise on practice

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+ 
 
-
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
     constructor(props) {
         super(props);
@@ -10,6 +13,7 @@ export default class SearchBar extends Component {
 
         // Prevent error causing by 'this'
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){
@@ -20,6 +24,9 @@ export default class SearchBar extends Component {
         event.preventDefault();
 
         // We need to go and fetch weather data
+
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
     }
 
     render(){
@@ -38,3 +45,8 @@ export default class SearchBar extends Component {
     }
 }
 
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
