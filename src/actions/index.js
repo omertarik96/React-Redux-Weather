4b977ba0af33156ea7fdcd1f087d0e05ca0dc757
Omer Tarik <omertarik96@gmail.com>
2017-08-29 19:57:43 -0500

    Lesson 57: API request with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
-const API_KEY = "617a68288aeaa6948c93c07743701a1f";
+import axios from 'axios';
 
+const API_KEY = "617a68288aeaa6948c93c07743701a1f";
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) { 
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
 
